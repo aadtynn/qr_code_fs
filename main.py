3c78bcfaf7cd515e29c8944275c0323c5f2f3a8c
aadityan <aadityanharindran@gmail.com>
2026-02-09 16:15:11 +0530

    Add QR to string function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PIL import Image
 from pyzbar.pyzbar import decode
 import os
 
+
 def string_to_qr():
 
     text =input("Enter text to convert into QR code : ")
@@ -26,15 +27,36 @@ def string_to_qr():
     filepath =f"{filename}.png"
     img.save(filepath)
     print("!!! QR code generated and saved as", filepath, "!!!")
+
+
+def qr_to_string():
+    filename =input("Enter file-name of the QR code (with extension) : ")
+
+    if not os.path.exists(filename):
+        print(f"!!! File not found - {filename} does not exist.")
+        return
     
+    try:
+        img =Image.open(filename)
+        decoded_objects =decode(img)
 
+        if not decoded_objects:
+            print("!!! No QR found in image.")
+            return
+        
+        print("QR code decoded successfully:")
+        for obj in decoded_objects:
+            print(f"Type : {obj.type}")
+            print(f"Data : {obj.data.decode('utf-8')}")
 
-
-
+    except Exception as e:
+        print(f"!!!Error reading image: {e}")
+        
+        
 
 def main():
     print("*" * 50 )
-    print("qr code generator")
+    print("QR CODE GENERATOR AND DECODER")
     print("*" * 50 )
     print("\n MENU")
     print("1. Text-to-QR code")
